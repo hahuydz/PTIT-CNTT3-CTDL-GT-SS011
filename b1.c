345b011eb1a80bbd825eff2d42ec852151690c59
hahuydz <haquanghuy2006abc@gmail.com>
2025-07-06 21:37:19 +0700

    b1.c

---
+++ b1.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node* prev;
+    struct Node* next;
+} Node;
+
+typedef struct DoublyLinkedList{
+    Node* head;
+    Node* tail;
+} DoublyLinkedList;
+
+void createList(DoublyLinkedList** list){
+    *list = (DoublyLinkedList*)malloc(sizeof(DoublyLinkedList));
+    (*list)->head = NULL;
+    (*list)->tail = NULL;
+}
+
+Node* createNode(int data){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+}
+
+int main(){
+    DoublyLinkedList* list = NULL;
+    createList(&list);
+    list->head = createNode(10);
+    Node* node1 = createNode(20);
+    Node* node2 = createNode(30);
+    Node* node3 = createNode(40);
+    list->tail = createNode(50);
+
+    list->head->next = node1;
+    node1->prev = list->head;
+    node1->next = node2;
+    node2->prev = node1;
+    node2->next = node3;
+    node3->prev = node2;
+    node3->next = list->tail;
+    list->tail = node3;
+    return 0;
+}
